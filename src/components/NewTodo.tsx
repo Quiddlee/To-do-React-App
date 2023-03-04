557f720e4b5b3be6630b1b3e3f1c3a8ad0d8ad7e
Bogdan <thegarrybit@gmail.com>
2023-03-04 18:14:58 +0200

    production

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -10,14 +10,17 @@ const NewTodo: React.FC<NewTodoProps> = props => {
 
     const todoSubmitHandler = (event: React.FormEvent) => {
         event.preventDefault();
-        const enteredText = textInputRef.current!.value;
-        props.onAddTodo(enteredText);
+        if (textInputRef.current && textInputRef.current.value) {
+            const enteredText = textInputRef.current.value;
+            props.onAddTodo(enteredText);
+            textInputRef.current.value = '';
+        }
     };
 
     return <form onSubmit={todoSubmitHandler}>
         <div className="form-control">
             <label htmlFor="todo-text">Todo Text</label>
-            <input type="text" id="todo-text" ref={textInputRef} />
+            <input placeholder="Add a task..." type="text" id="todo-text" ref={textInputRef} />
         </div>
         <button type="submit">Add Todo</button>
     </form>;
